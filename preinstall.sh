00abc58ffd5739d5f98b3923433cf80c4488e7cd
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2018-01-05 15:09:55 +0700

    Update preinstall script

--- preinstall.sh
+++ preinstall.sh
@@ -4,3 +4,18 @@
 #  Do Not Remove This File !                    #
 #  This is customize script before server up    # 
 # ============================================= #
+
+ENV='production'
+GEM=`which gem`
+BUNDLE=`which bundle`
+
+if [ "$ENV" = "production" ]
+then
+  PWD='/home/deploy/rb_deploy/current'
+else
+  PWD='/home/zeroc0d3/zeroc0d3-deploy/current'
+fi
+
+cd $PWD
+$GEM install bundle
+$BUNDLE install --binstubs
